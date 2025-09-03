d295e98369186b7f905e5b017d2be4937652421a
JulioGaspareto <juliogaspareto0@gmail.com>
2025-09-02 22:14:24 -0300

    aula react

---
+++ prof.Rafael/02-estado/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import Venda523 from './componets/Venda523/'
+import './App.css'
+
+function App() {
+    return (
+    <>
+    <div>
+  <Venda523 />
+    </div>
+    </>
+  )
+}
+
+export default App
+
+
+
+//       {username && <p> Seja bem vindo</p> }
+
+//     <p className='user'> ola{username}</p>
+//     <h1>Estado</h1>
+
+//       <button onClick={decrementar}>-</button>
+//       {numero}
+//       <button onClick={acrecentar}>+</button>
+//       <br />
+      
+// <button onClick={logar}>Logar</button> *
+
+
+
+//   // const[numero, setNumero] = useState(0)
+//   // const [username, setUsername] = useState('')
+
+//   // function logar (){
+//   //  let nome = prompt("digita teu nome")
+//   //   setUsername(nome)
+//   // }
+  
+//   // function acrecentar(){
+//   //   setNumero(numero + 1)
+//   // }
+
+//   //  function decrementar(){
+//   //   setNumero(numero - 1)
+  
